5510b8134fef47dd0b77201f49bb7c6da5a933df
Ren Paczkowski <rpaczkowski@gmail.com>
2023-12-18 16:57:21 -0500

    adding image to card

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import qrCode from "../src/images/image-qr-code.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
-      <footer>        
+      <div className="card-container">
+        <div className="card">
+          <div className="qr-container">
+            <img src={qrCode} />
+          </div>
+          <div className="bold-title">
+            <h3>Improve your front-end skills by building projects</h3>
+          </div>
+          <div className="subtext">
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
+        </div>
+      </div>
+      <footer>
         <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-    Coded by <a href="https://github.com/RenMari5/">Ren Paczkowski</a>.
-  </div></footer>
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/RenMari5/">Ren Paczkowski</a>.
+        </div>
+      </footer>
     </div>
   );
 }
